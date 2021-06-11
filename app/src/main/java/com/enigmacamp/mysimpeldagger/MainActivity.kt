34f8e6b7ce1855288a842230de9712f384410ff7
doni <donioctavian1016@gmail.com>
2021-06-11 15:36:05 +0700

    First_Set_No_Dagger_Implementation

--- app/src/main/java/com/enigmacamp/mysimpeldagger/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mysimpeldagger/MainActivity.kt
@@ -2,10 +2,17 @@ package com.enigmacamp.mysimpeldagger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val traineeInformation: TraineeInformation = TraineeInformation()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Trainee", traineeInformation.getListCustomer().toString())
+        Log.d("Trainee", traineeInformation.getCountCustomer().toString())
     }
 }
--- app/src/main/java/com/enigmacamp/mysimpeldagger/TraineeInformation.kt
+++ app/src/main/java/com/enigmacamp/mysimpeldagger/TraineeInformation.kt
@@ -1,4 +1,9 @@
 package com.enigmacamp.mysimpeldagger
 
 class TraineeInformation {
+    private val traineeDatabase = TraineeDatabase()
+
+    fun getListCustomer() = traineeDatabase.getList()
+
+    fun getCountCustomer() = traineeDatabase.getList().size
 }
